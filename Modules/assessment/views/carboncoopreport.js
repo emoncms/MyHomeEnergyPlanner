c85e75589f074e249e12f30334b970832ac32f9b
Harry Parton <hparton@hotmail.co.uk>
2015-09-15 11:39:17 +0100

    Swap out testing graphs for graphs found in the current carbon coop report

--- Modules/assessment/views/carboncoopreport.js
+++ Modules/assessment/views/carboncoopreport.js
@@ -5,139 +5,107 @@ function carboncoopreport_initUI() {
 
 	console.log(project);
 
-
-	// Test Chart
-
-	var chart = new BarChart({
-		chartTitle: 'Primary Energy Use',
-		yAxisLabel: 'kWh/m2.year',
-		font: 'Karla',
-		fontSize: 28,
-		chartLow: -100,
-		chartHigh: 400,
-		division: 100,
-		width: 1200,
-		chartHeight: 400,
-		barWidth: 140,
-		barGutter: 40,
-		defaultBarColor: 'rgb(30,160,30)',
-		ranges: [
-			{
-				low: 50,
-				high: 150,
-				color: 'rgb(254,204,204)'
-			}
-		],
-		barColors: {
-			'Space heating': 'rgb(65,168,198)',
-			'Pumps, fans, etc.': 'rgb(24,86,62)',
-			'Cooking': 'rgb(147,162,147)',
-			'Gas (total from bills)': 'rgb(236,102,79)',
-			'Solid fuel (total from bills)': 'rgb(246,167,7)',
-			'Total primary energy': 'rgb(241,138,157)',
-			'Water heating': 'rgb(82,41,57)',
-			'Lighting': 'rgb(10,175,154)'
-		},
-		data: [
-			{
-				label: 'UK Average',
-				value: [
-					{value: 50, label: 'Gas (total from bills)'},
-					{value: 80, label: 'Space heating'}
-				]
-			},
-			{
-				label: 'Label 1',
-				value: [
-					{value: 200, label: 'Gas (total from bills)'},
-					{value: 120, label: 'Space heating'},
-					{value: 65, label: 'Total primary energy'},
-					{value: 30, label: 'Solid fuel (total from bills)'}
-				]
-			},
-			{label: 'Your home now (model)', value: -80},
-			{label: 'Carbon Coop 2050 target', value: 125}
-		]
-	});
-
-	chart.draw('barchart');
-
 	// Fabric Energy Chart
 
-	var otherchart = new BarChart({
+
+
+	var FabricEnergy = new BarChart({
+		chartTitle: 'Fabric Energy',
 		yAxisLabel: 'kWh/m2.year',
 		fontSize: 22,
 		chartLow: -1,
-		chartHigh: 219,
-		division: 20,
-		width: 1200,
-		chartHeight: 500,
-		barWidth: 110,
-		barGutter: 80,
-		defaultBarColor: 'rgb(231,37,57)',
-		ranges: [
-			{
-				low: 60,
-				high: 100,
-				color: 'rgb(254,204,204)'
-			},
-		],
-		data: [
-			{label: 'UK Average', value: 140},
-			{label: '2000 B Regs Average', value: 60},
-			{label: 'Your home now', value: 180},
-			{label: 'Your 2050 home', value: 42},
-			{label: '2016 B Regs Average', value: 40}
-		]
-	});
-
-	otherchart.draw('barchart-2');
-
-	// Primary Energy Use Chart
-
-	var AnotherChart = new BarChart({
-		yAxisLabel: 'kWh/m2.year',
-		fontSize: 22,
-		chartLow: -129,
-		chartHigh: 619,
-		division: 80,
+		chartHigh: 169,
+		division: 10,
 		width: 1200,
 		chartHeight: 600,
 		barWidth: 110,
 		barGutter: 80,
 		defaultBarColor: 'rgb(231,37,57)',
-		barColors: {
-			'Space heating': 'rgb(65,168,198)',
-			'Pumps, fans, etc.': 'rgb(24,86,62)',
-			'Cooking': 'rgb(147,162,147)',
-			'Gas (total from bills)': 'rgb(236,102,79)',
-			'Solid fuel (total from bills)': 'rgb(246,167,7)',
-			'Total primary energy': 'rgb(241,138,157)',
-			'Water heating': 'rgb(82,41,57)',
-			'Lighting': 'rgb(10,175,154)'
-		},
-		targets: [
+		ranges: [
 			{
-				target: 0,
-				color: 'rgb(0,0,0)'
-			}
+				low: 20,
+				high: 50,
+				color: 'rgb(254,204,204)'
+			},
 		],
 		data: [
-			{label: 'UK Average', value: [
-					{value: 200, label: 'Gas (total from bills)'},
-					{value: 120, label: 'Space heating'},
-					{value: 65, label: 'Total primary energy'},
-					{value: 30, label: 'Solid fuel (total from bills)'}
-			]},
-			{label: '2000 B Regs Average', value: 60},
-			{label: 'Your home now', value: 180},
-			{label: 'UK Average', value: [
-					{value: -50, label: 'Gas (total from bills)'},
-					{value: 120, label: 'Space heating'},
-					{value: 65, label: 'Cooking'},
-					{value: 30, label: 'Solid fuel (total from bills)'}
-			]},
-			{label: '2016 B Regs Average', value: 40}
+			{label: 'UK Average', value: 140},
+			{label: '2000 B Regs Average', value: 56},
+			{label: 'Your home now', value: 0},
+			{label: 'Your 2050 home', value: 0},
+			{label: '2016 B Regs Average', value: 45}
+		]
+	});
+
+	FabricEnergy.draw('barchart-2');
+
+	// Primary Energy Use Chart
+
+	// var AnotherChart = new BarChart({
+	// 	yAxisLabel: 'kWh/m2.year',
+	// 	fontSize: 22,
+	// 	chartLow: -129,
+	// 	chartHigh: 619,
+	// 	division: 80,
+	// 	width: 1200,
+	// 	chartHeight: 600,
+	// 	barWidth: 110,
+	// 	barGutter: 80,
+	// 	defaultBarColor: 'rgb(231,37,57)',
+	// 	barColors: {
+	// 		'Space heating': 'rgb(65,168,198)',
+	// 		'Pumps, fans, etc.': 'rgb(24,86,62)',
+	// 		'Cooking': 'rgb(147,162,147)',
+	// 		'Gas (total from bills)': 'rgb(236,102,79)',
+	// 		'Solid fuel (total from bills)': 'rgb(246,167,7)',
+	// 		'Total primary energy': 'rgb(241,138,157)',
+	// 		'Water heating': 'rgb(82,41,57)',
+	// 		'Lighting': 'rgb(10,175,154)'
+	// 	},
+	// 	targets: [
+	// 		{
+	// 			target: 0,
+	// 			color: 'rgb(0,0,0)'
+	// 		}
+	// 	],
+	// 	data: [
+	// 		{label: 'UK Average', value: [
+	// 				{value: 200, label: 'Gas (total from bills)'},
+	// 				{value: 120, label: 'Space heating'},
+	// 				{value: 65, label: 'Total primary energy'},
+	// 				{value: 30, label: 'Solid fuel (total from bills)'}
+	// 		]},
+	// 		{label: '2000 B Regs Average', value: 60},
+	// 		{label: 'Your home now', value: 180},
+	// 		{label: 'UK Average', value: [
+	// 				{value: -50, label: 'Gas (total from bills)'},
+	// 				{value: 120, label: 'Space heating'},
+	// 				{value: 65, label: 'Cooking'},
+	// 				{value: 30, label: 'Solid fuel (total from bills)'}
+	// 		]},
+	// 		{label: '2016 B Regs Average', value: 40}
+	// 	]
+	// });
+
+	var AnotherChart = new BarChart({
+		chartTitle: 'Primary Energy Use',
+		yAxisLabel: 'kWh/m2.year',
+		fontSize: 22,
+		chartLow: -1,
+		chartHigh: 439,
+		division: 30,
+		width: 1200,
+		chartHeight: 600,
+		barWidth: 110,
+		barGutter: 60,
+		defaultBarColor: 'rgb(231,37,57)',
+		data: [
+			{label: 'UK Average', value: 377},
+			{label: 'Your home now (model)', value: 0},
+			{label: 'Your home now (bills)', value: 0},
+			{label: 'Your 2050 home (gross)', value: 0},
+			{label: 'Your 2050 home (net)', value: 0},
+			{label: 'Carbon Coop 2050 target', value: 124}
 		]
 	});
 
@@ -146,11 +114,12 @@ function carboncoopreport_initUI() {
 	// Carbon Dioxide Emissions Chart
 
 	var YetAnotherChart = new BarChart({
+		chartTitle: 'Carbon Dioxide Emissions',
 		yAxisLabel: 'kWh/m2.year',
 		fontSize: 22,
 		chartLow: -1,
-		chartHigh: 139,
-		division: 20,
+		chartHigh: 134,
+		division: 15,
 		width: 1200,
 		chartHeight: 600,
 		barWidth: 110,
@@ -163,36 +132,23 @@ function carboncoopreport_initUI() {
 		},
 		targets: [
 			{
-				target: 20,
-				color: 'rgb(0,0,0)'
+				label: '65kwH/m2.year',
+				target: 65,
+				color: 'rgb(231,37,57)'
 			},
 			{
-				target: 70,
+				label: '105kwH/m2.year',
+				target: 110,
 				color: 'rgb(231,37,57)'
 			},
 		],
-		ranges: [
-			{
-				high: 50,
-				low: 30,
-				color: 'rgb(254,204,204)'
-			},
-		],
 		data: [
-			{label: 'UK Average', value: 70},
-			{label: 'Your home now (model)', value: [
-					{value: 50, label: 'Space heating'},
-					{value: 10, label: 'Pumps, fans, etc.'}
-			]},
-			{label: 'Your home now (bills)', value: 56},
-			{label: 'Your 2050 home', value: [
-					{value: 10, label: 'Cooking'},
-					{value: 10, label: 'Pumps, fans, etc.'},
-			]},
-			{label: '2016 B Regs', value: [
-					{value: 20, label: 'Cooking'},
-			]},
-			{label: 'Carbon Coop 2050 Target', value: 40}
+			{label: 'UK Average', value: 100},
+			{label: 'Your home now (model)', value: 0},
+			{label: 'Your home now (bills)', value: 0},
+			{label: 'Your 2050 home', value: 0},
+			{label: '2016 B Regs', value: 12},
+			{label: 'Carbon Coop 2050 Target', value: 16}
 		]
 	});
 
