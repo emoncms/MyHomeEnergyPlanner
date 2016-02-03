bf52a6d130bdfc6331cc99f4e006305ac22aa602
cagabi <cagabi@lapiluka.org>
2016-02-03 12:33:07 +0000

    Issue 63: fixed. Only project['master'].currentenergy displayed

--- Modules/assessment/views/currentenergy.js
+++ Modules/assessment/views/currentenergy.js
@@ -1,6 +1,6 @@
 function currentenergy_initUI() {
 
-    var E = data.currentenergy.energyitems;
+    var E = project['master'].currentenergy.energyitems;
     console.log(E);
     var out = "";
     var lastgroup = "";
@@ -25,17 +25,17 @@ function currentenergy_initUI() {
             if (E[z].note != "")
                 out += "<br><i style='font-size:12px'>" + E[z].note + "</i>";
             out += "</td>";
-            out += "<td><input type='text' style='width:60px' key='data.currentenergy.energyitems." + z + ".quantity' /> " + E[z].units + "</td>";
+            out += "<td><input type='text' style='width:60px' key='project['master'].currentenergy.energyitems." + z + ".quantity' /> " + E[z].units + "</td>";
             if (E[z].mpg != undefined) {
-                out += "<td><input type='text' style='width:60px' key='data.currentenergy.energyitems." + z + ".mpg' /> mpg</td>";
+                out += "<td><input type='text' style='width:60px' key='project['master'].currentenergy.energyitems." + z + ".mpg' /> mpg</td>";
             } else {
                 out += "<td></td>";
             }
-            out += "<td><span type='text' key='data.currentenergy.energyitems." + z + ".kwhd' dp=1/> kWh/d</td>";
-            out += "<td><span type='text' key='data.currentenergy.energyitems." + z + ".annual_co2' dp=2/> kg</td>";
-            out += "<td><input type='text' style='width:50px' key='data.currentenergy.energyitems." + z + ".unitcost' dp=2 /> £/" + E[z].units + "</td>";
-            out += "<td><input type='text' style='width:50px' key='data.currentenergy.energyitems." + z + ".standingcharge' dp=2 /></td>";
-            out += "<td>£<span type='text' key='data.currentenergy.energyitems." + z + ".annual_cost' dp=2/></td>";
+            out += "<td><span type='text' key='project['master'].currentenergy.energyitems." + z + ".kwhd' dp=1/> kWh/d</td>";
+            out += "<td><span type='text' key='project['master'].currentenergy.energyitems." + z + ".annual_co2' dp=2/> kg</td>";
+            out += "<td><input type='text' style='width:50px' key='project['master'].currentenergy.energyitems." + z + ".unitcost' dp=2 /> £/" + E[z].units + "</td>";
+            out += "<td><input type='text' style='width:50px' key='project['master'].currentenergy.energyitems." + z + ".standingcharge' dp=2 /></td>";
+            out += "<td>£<span type='text' key='project['master'].currentenergy.energyitems." + z + ".annual_cost' dp=2/></td>";
             out += "<td><i class='currentenergy-deleteitem icon-trash' tag='" + z + "'></i></td></tr>";
         }
 
@@ -48,7 +48,7 @@ function currentenergy_UpdateUI()
     // ---------------------------------------------------------------------------------
     var options = {
         name: "Space heating demand",
-        value: Math.round(data.currentenergy.spaceheating_annual_kwhm2),
+        value: Math.round(project['master'].currentenergy.spaceheating_annual_kwhm2),
         units: "kWh/m2",
         targets: {
             //"Passivhaus": 15,
@@ -60,7 +60,7 @@ function currentenergy_UpdateUI()
     // ---------------------------------------------------------------------------------
     var options = {
         name: "Primary energy demand",
-        value: Math.round(data.currentenergy.primaryenergy_annual_kwhm2),
+        value: Math.round(project['master'].currentenergy.primaryenergy_annual_kwhm2),
         units: "kWh/m2",
         targets: {
             "Passivhaus": 120,
@@ -71,7 +71,7 @@ function currentenergy_UpdateUI()
     // ---------------------------------------------------------------------------------
     var options = {
         name: "CO2 Emission rate",
-        value: Math.round(data.currentenergy.total_co2m2),
+        value: Math.round(project['master'].currentenergy.total_co2m2),
         units: "kgCO2/m2",
         targets: {
             "80% by 2050": 17,
@@ -82,7 +82,7 @@ function currentenergy_UpdateUI()
     // ---------------------------------------------------------------------------------
     var options = {
         name: "Per person energy use",
-        value: data.currentenergy.energyuseperperson.toFixed(1),
+        value: project['master'].currentenergy.energyuseperperson.toFixed(1),
         units: "kWh/day",
         targets: {
             "70% heating saving": 8.6,
@@ -95,15 +95,15 @@ function currentenergy_UpdateUI()
 $("#add_energyitem").click(function () {
     var tag = $("#energyitem_select").val();
     console.log(tag);
-    data.currentenergy.energyitems[tag].selected = 1;
+    project['master'].currentenergy.energyitems[tag].selected = 1;
     currentenergy_initUI();
     update();
 });
 
 $("#currentenergy_energyitems").on("click", ".currentenergy-deleteitem", function () {
     var tag = $(this).attr("tag");
-    data.currentenergy.energyitems[tag].selected = 0;
-    data.currentenergy.energyitems[tag].quantity = 0;
+    project['master'].currentenergy.energyitems[tag].selected = 0;
+    project['master'].currentenergy.energyitems[tag].quantity = 0;
     currentenergy_initUI();
     update();
 });
