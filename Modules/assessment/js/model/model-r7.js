f4ae4763bf881b51ccd5a71965097c935d70904b
cagabi <cagabi@lapiluka.org>
2016-05-11 16:09:04 +0100

    Fans and pumps was not taking into account ventilation properly

--- Modules/assessment/js/model/model-r7.js
+++ Modules/assessment/js/model/model-r7.js
@@ -1710,7 +1710,30 @@ calc.fans_and_pumps = function (data) {
     }
 
     // From Ventilation (SAP2012 document page 213)
-    switch (data.ventilation.ventilation_type) {
+    var ventilation_type = '';
+    switch (data.ventilation.ventilation_type)
+    {
+        case 'NV':
+        case 'IE':
+        case 'PS':
+            ventilation_type = 'd'; // Natural ventilation or whole house positive input ventilation from loft'
+            break;
+        case 'DEV':
+        case'MEV':
+            ventilation_type = 'c'; // Whole house extract ventilation or positive input ventilation from outside
+            break;
+        case 'MV':
+            ventilation_type = 'b'; // Balanced mechanical ventilation without heat recovery (MV)
+            break;
+        case 'MVHR':
+            ventilation_type = 'a'; //Balanced mechanical ventilation with heat recovery (MVHR)
+            break;
+        default:
+            data.ventilation.ventilation_type = 'NV';
+            ventilation_type = 'd';
+            break;
+    }
+    switch (ventilation_type) {
         case 'd':  //Positive input ventilation (from loft space)
             // Do nothing - In this case annual energy is 0, see SAP2012 2.6.1: The energy used by the fan is taken as counterbalancing the effect of using slightly warmer air from the loft space compared with outside
             break;
