3105cbbb52d3aa9fd27fea3d61c2e4245bcbe142
cagabi <cagabi@lapiluka.org>
2016-05-11 16:13:50 +0100

    Internal gains was not taking properly into account ventilation

--- Modules/assessment/js/model/model-r7.js
+++ Modules/assessment/js/model/model-r7.js
@@ -1787,8 +1787,30 @@ calc.gains = function (data) {
     data.gains_W['fans_and_pumps'] = new Array();
     // Note: From if there was an oil boiler with pump inside dweling we should add 10W of gains, the problem is that i don't know where in MHEP we can as this. Therefor we assume taht in the case of havin an oil boiler the pump is outside :(
 
-    // From ventilation
-    switch (data.ventilation.ventilation_type) {
+    // From ventilationvar ventilation_type = '';
+    switch (data.ventilation.ventilation_type)
+    {
+        case 'NV':
+        case 'IE':
+        case 'PS':
+            ventilation_type = 'd'; // Natural ventilation or whole house positive input ventilation from loft'
+            break;
+        case 'DEV':
+        case'MEV':
+            ventilation_type = 'c'; // Whole house extract ventilation or positive input ventilation from outside
+            break;
+        case 'MV':
+            ventilation_type = 'b'; // Balanced mechanical ventilation without heat recovery (MV)
+            break;
+        case 'MVHR':
+            ventilation_type = 'a'; //Balanced mechanical ventilation with heat recovery (MVHR)
+            break;
+        default:
+            data.ventilation.ventilation_type = 'NV';
+            ventilation_type = 'd';
+            break;
+    }
+    switch (ventilation_type) {
         case 'a':  //Balanced mechanical ventilation with heat recovery (MVHR), the heat gains in this case are included in the MVHR efficiency
         case 'd':  //Positive input ventilation (from loft space)
             // Do nothing 
