40d5366a5441be132e29bb51fc1931c8d308116d
cagabi <cagabi@lapiluka.org>
2017-05-25 11:34:30 +0100

    datasets upgraded

--- Modules/assessment/view.php
+++ Modules/assessment/view.php
@@ -12,7 +12,7 @@ global $reports;
 <script language="javascript" type="text/javascript" src="<?php echo $d; ?>js/ui-openbem-r3.js"></script>
 
 <script language="javascript" type="text/javascript" src="<?php echo $d; ?>js/model/library-r6.js"></script>
-<script language="javascript" type="text/javascript" src="<?php echo $d; ?>js/model/datasets-r4.js"></script>
+<script language="javascript" type="text/javascript" src="<?php echo $d; ?>js/model/datasets-r5.js"></script>
 <script language="javascript" type="text/javascript" src="<?php echo $d; ?>js/model/model-r9.js"></script>
 <script language="javascript" type="text/javascript" src="<?php echo $d; ?>graph-r3.js"></script>
 <script language="javascript" type="text/javascript" src="<?php echo $d; ?>js/targetbar-r3.js"></script>
