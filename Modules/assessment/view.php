50ba64693500f3d27c5b2eef3276e35addfa5d8f
cagabi <cagabi@lapiluka.org>
2017-05-25 14:07:35 +0100

    Fixed problem when importing a house without household

--- Modules/assessment/view.php
+++ Modules/assessment/view.php
@@ -393,10 +393,12 @@ global $reports;
         console.log(scenario)
 
         // We need to calculate the periods of heating off here because if we try to do it in household.js it happens after the update
-         for (var s in project){ // we ensure all the scenarios have the same household data and heating off periods
-            project[s].household = project.master.household;
-            project[s].temperature.hours_off.weekday = get_hours_off_weekday(project[s]); 
-            project[s].temperature.hours_off.weekend = get_hours_off_weekend(project[s]);
+        if (project.master.household != undefined) {
+            for (var s in project) { // we ensure all the scenarios have the same household data and heating off periods
+                project[s].household = project.master.household;
+                project[s].temperature.hours_off.weekday = get_hours_off_weekday(project[s]);
+                project[s].temperature.hours_off.weekend = get_hours_off_weekend(project[s]);
+            }
         }
 
         project[scenario] = calc.run(project[scenario]);
