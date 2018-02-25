0083180642efacf18a5b91e8cf8956da190d7b06
TrystanLea <glyn.hudson@openenergymonitor.org>
2018-02-25 09:53:05 +0000

    mhep as an emoncms module

--- Modules/admin/admin_menu.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $domain = "messages";
-    bindtextdomain($domain, "Modules/admin/locale");
-    bind_textdomain_codeset($domain, 'UTF-8');
-
-    $menu_dropdown_config[] = array('name'=> dgettext($domain, "Administration"), 'icon'=>'icon-tasks', 'path'=>"admin/view" , 'session'=>"admin", 'order' => 50 );
