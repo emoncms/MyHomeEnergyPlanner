a3c051f6dc6dc523a97270e61bca8d403a0b6e9a
TrystanLea <glyn.hudson@openenergymonitor.org>
2018-02-26 12:25:42 +0000

    fix left hand position of page

--- view.php
+++ view.php
@@ -809,8 +809,8 @@ function show_sidebar() {
     var width = $(window).width();
     sidebar_visible = true;
     $("#sidebar").css("left","340px");
-    //if (width>=max_wrapper_width) $("#wrapper").css("padding-left","250px");
-    //$("#wrapper").css("margin","0");
+    if (width>=max_wrapper_width) $("#wrapper").css("padding-left","330px");
+    $("#wrapper").css("margin","0");
     $("#sidenav-open").hide();
     $("#sidenav-close").hide();
 }
