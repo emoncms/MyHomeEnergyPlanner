45b56b2e09cdf65cf45c2f18270e85a40f29d55e
TrystanLea <glyn.hudson@openenergymonitor.org>
2018-02-26 09:47:39 +0000

    menu items

--- assessment_menu.php
+++ assessment_menu.php
@@ -8,7 +8,27 @@
         'path'=>"assessment/list" , 
         'session'=>"write", 
         'order' => 0,
-        'icon'=>'icon-home icon-white'
+        'icon'=>'icon-home icon-white',
+        'hideinactive'=>1
+    );
+    
+    $menu_left[] = array(
+        'id'=>"assessment_menu",
+        'name'=>"MyHomeEnergyPlanner", 
+        'path'=>"assessment/view" , 
+        'session'=>"write", 
+        'order' => 0,
+        'icon'=>'icon-home icon-white',
+        'hideinactive'=>1
+    );
+
+    $menu_dropdown[] = array(
+        'id'=>"assessment_menu_extras",
+        'name'=>"MyHomeEnergyPlanner", 
+        'path'=>"assessment/list" , 
+        'session'=>"write", 
+        'order' => 0,
+        'icon'=>'icon-home'
     );
     
     
