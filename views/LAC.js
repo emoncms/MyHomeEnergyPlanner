2ea1aa3570891591f49b9dae7c07c5ae3812ee5c
cagabi <carlos@carbon.coop>
2019-06-12 12:13:17 +0100

    Fixed #416 bug that allows to reaply measures to lighting which made the quantitiy of LE bulbs in the measure to be 0

--- views/LAC.js
+++ views/LAC.js
@@ -31,6 +31,10 @@ function LAC_initUI() {
             $('#LAC-cooking-fuels').append(out);
         }
     }
+    
+    if(scenario != "master" && data.LAC.L == data.LAC.LLE)
+        $('#apply-measure-lighting').attr("disabled", "true");
+    
     // Show "Measured applied" in lighting
     if (data.measures != undefined && data.measures.LAC != undefined && data.measures.LAC.lighting != undefined)
         $('#lighting-measure-applied').show();
